e64b9c93d94b0a97bd4aea31dd9261dff92aa219
Sir Pineapple <dhruvgupta376@gmail.com>
2026-01-17 03:37:32 +0530

    Created Media Schema for local media indexing

---
+++ backend/src/models/Media.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const MediaSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        year: {
+            type: String,
+        },
+        type: {
+            type: String,
+            enum: ["movie", "episode"],
+            required: true,
+        },
+        filePath: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        extension: {
+            type: String,
+            required: true,
+        },
+        folder: {
+            type: String,
+            required: true,
+        },
+        poster: {
+            type: String,
+        },
+        imdbRating: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Media", MediaSchema);
