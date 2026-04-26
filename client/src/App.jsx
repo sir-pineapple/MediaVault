fda87604c6746fc9efaffd70fff00c8a5d8adc2d
sir-pineapple <dhruvgupta376@gmail.com>
2026-04-26 19:19:51 +0530

    initialized frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+        Tailwind working
+    </div>
+  );
+}
